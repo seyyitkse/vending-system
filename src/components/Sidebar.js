75383ac2f4b1d053d3abcc5c246e8b3aa66fdb80
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-10-31 09:56:05 +0300

    Refactor Sidebar component and styles to improve layout and add accordion functionality for role management

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom'; // Import useLocation to track current route
+import { Link, useLocation } from 'react-router-dom';
 import { GiVendingMachine } from "react-icons/gi";
-import { FaHome, FaBox, FaChartBar, FaClipboardList, FaUser, FaCogs ,FaUsers } from 'react-icons/fa';
-// import { SiAwselasticloadbalancing } from "react-icons/si";
+import { FaHome, FaBox, FaChartBar, FaClipboardList, FaUser, FaCogs, FaUsers } from 'react-icons/fa';
 import { GiSecretBook } from "react-icons/gi";
-
-import './Sidebar.css'; // CSS file
+import { PiTreeStructureDuotone } from "react-icons/pi";
+import { TbUsersGroup } from "react-icons/tb";
+import './Sidebar.css';
 
 function Sidebar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const location = useLocation(); // Hook to get the current route
+  const [isRoleOpen, setIsRoleOpen] = useState(false);
 
-  // Function to check if the current path matches the route
+  const location = useLocation();
+
   const isActive = (path) => location.pathname === path;
 
   return (
     <div className="sidebar">
       <h2 className="sidebar-title">Otomat Takip</h2>
       <ul>
+        {/* Menu items */}
         <li className={isActive('/') ? 'active' : ''}>
           <Link to="/"><FaHome /> Ana Sayfa</Link>
         </li>
@@ -36,12 +38,27 @@ function Sidebar() {
         <li className={isActive('/vending-machine-registration') ? 'active' : ''}>
           <Link to="/vending-machine-registration"><GiVendingMachine /> Otomat Kayıt</Link>
         </li>
-        <li className={isActive('/staff-management') ? 'active' : ''}>
-          <Link to="/staff-management"><FaUsers /> Çalışan Yönetimi</Link>
-        </li>
         <li className={isActive('/log-records') ? 'active' : ''}>
           <Link to="/log-records"><GiSecretBook /> Log Kayıtları</Link>
         </li>
+        <li className={isActive('/departments/list') ? 'active' : ''}>
+          <Link to="/departments/list"><PiTreeStructureDuotone /> Departmanlar</Link>
+        </li>
+        <li onClick={() => setIsRoleOpen(!isRoleOpen)} className="accordion-header">
+          <div className="accordion-toggle">
+            <TbUsersGroup />
+            <span>Roller</span>
+          </div>
+          <span className={isRoleOpen ? 'arrow open' : 'arrow'}>&#9660;</span>
+        </li>
+        <ul className={`accordion-content ${isRoleOpen ? 'open' : ''}`}>
+        <li className={isActive('/roles') ? 'active' : ''}>
+        <Link to="/roles"><TbUsersGroup /> Roller</Link>
+        </li>
+        <li className={isActive('/user-management') ? 'active' : ''}>
+          <Link to="/user-management"><FaUsers /> Kullanıcı Rolleri</Link>
+        </li>
+        </ul>
       </ul>
       <div 
         className="user-info"
